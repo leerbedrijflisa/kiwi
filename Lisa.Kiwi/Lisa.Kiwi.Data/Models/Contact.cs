5ace4bfaa78b8cb79f3895853476bb71bbe0b01f
Jordi Scharloo <jordischarloo@gmail.com>
2014-09-18 11:18:49 +0100

    Add Report and Contact data models

---
+++ Lisa.Kiwi/Lisa.Kiwi.Data/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lisa.Kiwi.Data.Models
+{
+    class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public int? StudentNo { get; set; }
+    }
+}
---
+++ Lisa.Kiwi/Lisa.Kiwi.Data/Models/Report.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lisa.Kiwi.Data.Models
+{
+    class Report
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+
+        public const string Status = "Open";
+
+        public DateTime Created { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public string Guid { get; set; }
+
+        public string UserAgent { get; set; }
+
+        [MaxLength(45)]
+        public string Ip { get; set; }
+
+        public enum Type
+        {
+            Drugs,
+            Overlast,
+            Voertuigen,
+            Inbraak,
+            Diefstal,
+            Intimidatie,
+            Pesten,
+            Digitaal
+        }
+
+        public virtual ICollection<Contact> Contact { get; set; }
+
+        public Report()
+        {
+            Contact = new List<Contact>();
+        }
+    }
+}
