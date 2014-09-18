5cdd43e7cae5f3b79b60ebf2e1bc386206270fd8
Frank van Driel <frank.vandriel@hotmail.com>
2014-09-18 15:18:58 +0200

    Commit Dashboard view and report view

--- Lisa.Kiwi/Lisa.Kiwi.Web.Dashboard/App_Start/RouteConfig.cs
+++ Lisa.Kiwi/Lisa.Kiwi.Web.Dashboard/App_Start/RouteConfig.cs
@@ -16,7 +16,13 @@ namespace Lisa.Kiwi.Web.Dashboard
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Index", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Report",
+                url: "report/{controller}/{action}/{id}",
+                defaults: new { controller = "Report", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
