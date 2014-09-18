540201a834087049b4c2a0d49735b828910d9a24
Jordi Scharloo <jordischarloo@gmail.com>
2014-09-18 13:54:57 +0100

    Add Remark and Status data models

---
+++ Lisa.Kiwi/Lisa.Kiwi.Data/Models/Status.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lisa.Kiwi.Data.Models
+{
+    class Status
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public enum Name
+        {
+            Open,
+            Afgehandeld,
+            Bezig,
+            Doorgeschakelt
+        }
+
+        public virtual Report Report { get; set; }
+    }
+}
