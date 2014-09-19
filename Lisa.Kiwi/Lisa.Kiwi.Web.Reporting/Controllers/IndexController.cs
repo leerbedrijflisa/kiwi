e9f943387a2465b741f80c477a5a6121f27b9dcd
Marijn Kok <marijnkok94@gmail.com>
2014-09-19 16:02:10 +0200

    Lisa.Kiwi.Web.Reporting lots of forms and field, mostly prepared for the Data Access Layer, to talk with the WebApi.

---
+++ Lisa.Kiwi/Lisa.Kiwi.Web.Reporting/Controllers/IndexController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lisa.Kiwi.Web.Reporting.Models;
+using Lisa.Kiwi.Tools;
+
+namespace Lisa.Kiwi.Web.Reporting.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ReportType()
+        {
+            List<string> types = new List<string>()
+            {
+                "Drugs", "Overlast", "Voertuigen", "Inbraak", "Diefstal", "Intimidatie", "Pesten", "Digitaal grensoverschrijdend gedrag", "Etc"
+            };
+
+            ViewBag.ReportTypes = types;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Report(OriginalReport report)
+        {
+            var rand = new Random().Next(0000, 9999);
+            var id = "M" + DateTime.Now.ToString("yyyyMMdd") + "-" + DateTime.Now.ToString("HHmmss") + "-" + rand;
+            report.GuId = id;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ReportDetails(OriginalReport report, string contactMe)
+        {
+            if (!CheckReport(report))
+            {
+                return View("Report");
+            }
+
+            if (contactMe != "false")
+            {
+                return ContactDetails(report.GuId);
+            }
+
+            return View();
+        }
+
+        public ActionResult ContactDetails(string guid)
+        {
+
+            return View();
+        }
+
+        private bool CheckReport(OriginalReport report, bool checkEmpty = true, string[] skipField = null)
+        {
+            if (!ModelState.IsValid)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        
+    }
+}
---
+++ Lisa.Kiwi/Lisa.Kiwi.Web.Reporting/Tools/StringHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lisa.Kiwi.Tools
+{
+    public  class Utils
+    {
+        public static string GetIP()
+        {
+            string ipAddress;
+            HttpContext context = System.Web.HttpContext.Current;
+            ipAddress = context.Request.ServerVariables["REMOTE_ADDR"];
+            if (!string.IsNullOrEmpty(ipAddress))
+            {
+                return ipAddress;
+            }
+
+            ipAddress = context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+            if (!string.IsNullOrEmpty(ipAddress))
+            {
+                string[] addresses = ipAddress.Split(',');
+                if (addresses.Length != 0)
+                {
+                    return addresses[0];
+                }
+            }
+
+            return null;
+        }
+    }
+}
