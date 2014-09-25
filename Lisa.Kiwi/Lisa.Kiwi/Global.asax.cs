4c000a4b8caa9ad6ac52434c2250ccd09fc6d227
Jordi Scharloo <jordischarloo@gmail.com>
2014-09-25 11:47:56 +0100

    Redo changes that Frank undid in his merge :s

--- Lisa.Kiwi/Lisa.Kiwi/Global.asax.cs
+++ Lisa.Kiwi/Lisa.Kiwi/Global.asax.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Data.Entity;
 using System.Web;
 using System.Web.Http;
-using System.Web.Routing;
-using System.Data.Entity;
 using Lisa.Kiwi.Data.Models;
 
 namespace Lisa.Kiwi.WebApi
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
             Database.SetInitializer<KiwiContext>(null);
-
         }
     }
 }
