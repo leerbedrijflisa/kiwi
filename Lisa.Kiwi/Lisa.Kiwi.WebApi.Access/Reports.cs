6abbaaceb32ce54948e6264a7789cc1a60111a18
Marijn Kok <marijnkok94@gmail.com>
2014-10-01 09:29:34 +0200

    Create dump data, for dashboard.

---
+++ Lisa.Kiwi/Lisa.Kiwi.WebApi.Access/Reports.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lisa.Kiwi.WebApi.Access
+{
+    public class Reports
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public string Guid { get; set; }
+        public string UserAgent { get; set; }
+        public string Ip { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Time { get; set; }
+        public ReportPriority Priority { get; set; }
+    }
+
+    public enum ReportPriority
+    {
+        // TODO: translate
+        Laag,
+        Normaal,
+        Hoog
+    }
+}
