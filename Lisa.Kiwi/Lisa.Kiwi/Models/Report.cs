4ba051a66bab9c52d8cdce8c7f4222c4ad778013
Jordi Scharloo <jordischarloo@gmail.com>
2014-10-01 15:14:50 +0100

    Add WebApi report model to map and return

---
+++ Lisa.Kiwi/Lisa.Kiwi/Models/Report.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Lisa.Kiwi.Data.Models;
+
+namespace Lisa.Kiwi.WebApi.Models
+{
+    public class Report
+    {
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public string Location { get; set; }
+        public DateTime Time { get; set; }
+        public string Guid { get; set; }
+        public string UserAgent { get; set; }
+        public string Ip { get; set; }
+        public StatusName Status { get; set; }
+    }
+}
