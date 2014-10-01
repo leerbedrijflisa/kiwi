12219f30914b178c8a7fb354ae5651ceac2762a9
Frank van Driel <frank.vandriel@hotmail.com>
2014-10-01 09:08:24 +0200

    added a method for get all reports in the access layer

--- Lisa.Kiwi/Lisa.Kiwi.WebApi.Access/Connector.cs
+++ Lisa.Kiwi/Lisa.Kiwi.WebApi.Access/Connector.cs
@@ -3,25 +3,37 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
+
 namespace Lisa.Kiwi.WebApi.Access
 {
+    public class Report
+    {
+        public string Description { get; set; }
+    }
+
     public class Connector
     {
         public static HttpClient Client;
 
-        public static void Main() {
-            RunAsync().Wait();
-        }
 
-        public static async Task RunAsync() {
+        public async Task<Report[]> GetAllReports()
+        {
+                using (Client = new HttpClient())
+                {
+                    Client.BaseAddress = new Uri("http://localhost:20151/");
+                    Client.DefaultRequestHeaders.Accept.Clear();
+                    Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            using (Client = new HttpClient())
-            {
-                Client.BaseAddress = new Uri("http://localhost:20151/");
-                Client.DefaultRequestHeaders.Accept.Clear();
-                Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
+                    // HTTP GET Reports
+                    HttpResponseMessage reports = await Client.GetAsync("odata/Report");
+                    if (reports.IsSuccessStatusCode)
+                    {
+                        Report[] result = await reports.Content.ReadAsAsync<Report[]>();
+                        return result;
+                    }
+                    return null;
+                }
             }
-        }
+        
     }
 }
