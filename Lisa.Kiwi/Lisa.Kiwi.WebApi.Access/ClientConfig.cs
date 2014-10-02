f97d9b07364eb409e4877a2847bda88ae41db95f
Jordi Scharloo <jordischarloo@gmail.com>
2014-10-02 10:23:29 +0100

    Access layer skeleton, added service reference, add odata v4 client setup

---
+++ Lisa.Kiwi/Lisa.Kiwi.WebApi.Access/ClientConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Lisa.Kiwi.WebApi.Access
+{
+    class ClientConfig
+    {
+        //Where is the OData API hosted?
+        private const string BaseUrl = "http://localhost:20151/";
+
+        public HttpClient BuildClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(BaseUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+    }
+}
---
+++ Lisa.Kiwi/Lisa.Kiwi.WebApi.Access/Models/Report.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lisa.Kiwi.WebApi.Access.Models
+{
+    public class Report
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public string Location { get; set; }
+        public DateTime Time { get; set; }
+        public string Guid { get; set; }
+        public string UserAgent { get; set; }
+        public string Ip { get; set; }
+    }
+}
