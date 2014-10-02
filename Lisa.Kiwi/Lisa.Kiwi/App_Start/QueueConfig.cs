acdf007a8f0423a3a2debcab5066ae051eef7ee5
Jordi Scharloo <jordischarloo@gmail.com>
2014-10-02 10:17:37 +0100

    Add odata client dependencies to Access layer project

--- Lisa.Kiwi/Lisa.Kiwi/App_Start/QueueConfig.cs
+++ Lisa.Kiwi/Lisa.Kiwi/App_Start/QueueConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Microsoft.WindowsAzure;
+﻿using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
 
