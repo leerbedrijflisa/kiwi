634197549ecb7613fab7b7ab8cdb14b2c89a705a
Marijn Kok <marijnkok94@gmail.com>
2014-10-02 09:07:22 +0200

    Moved Report details to the Report controller.

--- Lisa.Kiwi/Lisa.Kiwi.Web.Dashboard/Controllers/DashboardController.cs
+++ Lisa.Kiwi/Lisa.Kiwi.Web.Dashboard/Controllers/DashboardController.cs
@@ -5,7 +5,6 @@ using System.Web;
 using System.Web.Mvc;
 using Lisa.Kiwi.WebApi.Access;
 using System.Threading.Tasks;
-using Lisa.Kiwi.Web.Dashboard.Models;
 
 namespace Lisa.Kiwi.Web.Dashboard.Controllers
 {
@@ -27,25 +26,6 @@ namespace Lisa.Kiwi.Web.Dashboard.Controllers
                 .OrderBy(r => r.Created);
 
             return View(reportsData);          
-        }
-
-        public ActionResult Report(string id)
-        {
-            var sessionTimeOut = Session.Timeout = 60;
-            if (Session["user"] == null || sessionTimeOut == 0)
-            {
-                return RedirectToAction("Login", "Account");
-            }
-
-            var report = Reports.GetReport(id);
-            var statusses = new SelectList(Enum.GetValues(typeof(StatusName)).Cast<StatusName>().Select(v => new SelectListItem
-             {
-                 Text = v.ToString(),
-                 Value = ((int)v).ToString()
-             }).ToList(), "Value", "Text");
-            ViewBag.Statusses = statusses;
-
-            return View(report);
         }
     }
 }
--- Lisa.Kiwi/Lisa.Kiwi.Web.Dashboard/Controllers/ReportController.cs
+++ Lisa.Kiwi/Lisa.Kiwi.Web.Dashboard/Controllers/ReportController.cs
@@ -4,24 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Lisa.Kiwi.WebApi.Access;
+using System.Threading.Tasks;
 
 namespace Lisa.Kiwi.Web.Dashboard.Controllers
 {
     public class ReportController : Controller
     {
-        // GET: Report
-        public ActionResult Index()
+
+        private TempReports Reports = new TempReports();
+
+        public ActionResult Index(string id)
         {
             var sessionTimeOut = Session.Timeout = 60;
-
             if (Session["user"] == null || sessionTimeOut == 0)
             {
                 return RedirectToAction("Login", "Account");
             }
-            else
+
+            var report = Reports.GetReport(id);
+            var statusses = new SelectList(Enum.GetValues(typeof(StatusName)).Cast<StatusName>().Select(v => new SelectListItem
             {
-                return View("report");
-            }
+                Text = v.ToString(),
+                Value = ((int)v).ToString()
+            }).ToList(), "Value", "Text");
+            ViewBag.Statusses = statusses;
+
+            return View(report);
         }
     }
 }
