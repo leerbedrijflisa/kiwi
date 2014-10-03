6167030c8676085f9b1efabb457ed59fde37483a
William Willing <william@williamwilling.com>
2014-10-03 15:12:22 +0200

    Moved the login page to /Login.

--- Lisa.Kiwi/Lisa.Kiwi.Web.Dashboard/App_Start/RouteConfig.cs
+++ Lisa.Kiwi/Lisa.Kiwi.Web.Dashboard/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace Lisa.Kiwi.Web.Dashboard
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Login",
+                url: "Login",
+                defaults: new { controller = "Account", action = "Login" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Report", action = "Index", id = UrlParameter.Optional }
