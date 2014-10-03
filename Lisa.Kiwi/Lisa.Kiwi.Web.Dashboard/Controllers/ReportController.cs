2bae1973b21dbd322345812f2147865db427a2d1
Jordi Scharloo <jordischarloo@gmail.com>
2014-10-03 13:35:51 +0200

    Redo updating statuses and adding remarks

--- Lisa.Kiwi/Lisa.Kiwi.Web.Dashboard/Controllers/ReportController.cs
+++ Lisa.Kiwi/Lisa.Kiwi.Web.Dashboard/Controllers/ReportController.cs
@@ -29,7 +29,7 @@ namespace Lisa.Kiwi.Web.Dashboard.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Index(int id, string guid, string status, string comment)
+        public ActionResult Index(int id, string status, string comment)
         {
             var sessionTimeOut = Session.Timeout = 60;
             if (Session["user"] == null || sessionTimeOut == 0)
@@ -37,14 +37,8 @@ namespace Lisa.Kiwi.Web.Dashboard.Controllers
                 return RedirectToAction("Login", "Account");
             }
 
-            var reports = ReportProxy.GetReports();
 
-            var report = reports.Where(r => r.Id == id);
-
-            var statusses = Enum.GetValues(typeof(StatusName)).Cast<StatusName>().ToList();
-            ViewBag.Statusses = statusses;
-
-            return View(report);
+            return RedirectToAction("Report", "Index", id);
         }
     }
 }
