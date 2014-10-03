24e71c1b86b36db2ef87d29facc8070a473de1bf
William Willing <william@williamwilling.com>
2014-10-03 09:16:30 +0200

    Moved authorization check to before data access.

--- Lisa.Kiwi/Lisa.Kiwi.Web.Dashboard/Controllers/DashboardController.cs
+++ Lisa.Kiwi/Lisa.Kiwi.Web.Dashboard/Controllers/DashboardController.cs
@@ -6,14 +6,13 @@ namespace Lisa.Kiwi.Web.Dashboard.Controllers
     {
         public ActionResult Index()
         {
-            var reportsData = WebApi.Access.Proxies.Report.GetAllReports();
-
             var sessionTimeOut = Session.Timeout = 60;
             if (Session["user"] == null || sessionTimeOut == 0)
             {
                 return RedirectToAction("Login", "Account");
             }
 
+            var reportsData = WebApi.Access.Proxies.Report.GetAllReports();
             return View(reportsData);
         }
     }
