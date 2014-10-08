cbf19f1e2bc3b9f2a5f5cf3c0f2ab50f4693dc2c
Jordi Scharloo <jordischarloo@hotmail.com>
2014-10-08 17:15:48 +0200

    Add RemarkProxy for Access layer

---
+++ Lisa.Kiwi/Lisa.Kiwi.WebApi.Access/Proxies/StatusProxy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Lisa.Kiwi.WebApi.Access
+{
+    public class StatusProxy
+    {
+        // You need this to initialize the access layer
+        private static readonly Container container = new Container(new Uri(ClientConfig.BaseUrl));
+
+        // Get an entire entity set.
+        public static IQueryable<Status> GetStatuses()
+        {
+            return container.Status;
+        }
+
+        //Create a new entity
+        public static void AddStatus(Status status)
+        {
+            container.AddToStatus(status);
+            var serviceResponse = container.SaveChanges();
+        }
+    }
+}
