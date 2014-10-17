7f97582e16f3796f8bd23c3eb2f66abad9cdcca0
Jordi Scharloo <jordischarloo@hotmail.com>
2014-10-17 14:59:06 +0200

    whatthecommit.com

---
+++ Lisa.Kiwi/Lisa.Kiwi.WebApi.Access/Proxies/ContactProxy.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Default;
+
+//Namespace mimic door access layer template
+using Lisa.Kiwi.Data;
+
+namespace Lisa.Kiwi.WebApi.Access
+{
+    public class ContactProxy
+    {
+        // Get an entire entity set.
+        public static IQueryable<Contact> GetContacts()
+        {
+            return Client.Container.Contact;
+        }
+
+        //Create a new entity
+        public static void AddContact(Contact contact)
+        {
+            Client.Container.AddToContact(contact);
+            Client.Container.SaveChanges();
+            Client.Container = new Container(Client.BaseUri);
+        }
+    }
+}
