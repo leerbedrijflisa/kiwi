5cfda1fd72757709d0b4f8faf1e376153fa33dab
Layl Conway <laylconway@live.com>
2014-11-06 10:25:18 +0100

    Added an owin startup class.

---
+++ Lisa.Kiwi/Lisa.Kiwi/App_Start/OwinStartup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Lisa.Kiwi.WebApi.OwinStartup))]
+
+namespace Lisa.Kiwi.WebApi
+{
+    public class OwinStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
