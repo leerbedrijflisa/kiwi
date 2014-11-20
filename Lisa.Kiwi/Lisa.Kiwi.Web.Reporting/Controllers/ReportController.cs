77ced15e54f9458fc300c0beb78a3f3ccbcddf9c
Marijn kok <marijnkok94@gmail.com>
2014-11-20 14:22:25 +0100

    Refactor Reporting.ReportController

--- Lisa.Kiwi/Lisa.Kiwi.Web.Reporting/Controllers/ReportController.cs
+++ Lisa.Kiwi/Lisa.Kiwi.Web.Reporting/Controllers/ReportController.cs
@@ -40,20 +40,8 @@ namespace Lisa.Kiwi.Web.Reporting.Controllers
 
 		public ActionResult Type()
 		{
-			var types = Enum.GetValues(typeof (ReportType)).Cast<ReportType>();
-			List<SelectListItem> reportTypes = new List<SelectListItem>();
-			foreach (var reportType in types)
-			{
-				reportTypes.Add(new SelectListItem
-				{
-					Text = reportType.GetReportTypeDisplayNameFromMetadata(),
-					Value = reportType.ToString()
-				});
-			}
-
-
+            var reportTypes = GetReportTypes();
 			ViewData["reportType"] = reportTypes;
-
 
 			return View();
 		}
@@ -65,22 +53,14 @@ namespace Lisa.Kiwi.Web.Reporting.Controllers
 			{
 				CloudTable table = GetTableStorage();
 
-				string guid = Guid.NewGuid().ToString();
-
-				OriginalReport report = new OriginalReport();
-
-				report.Guid = guid;
-				report.Time = DateTime.UtcNow;
-                report.Type = reportType.ToString();
-				report.PartitionKey = guid;
-				report.RowKey = "";
+                var report = DefineReport(reportType);
 
 				TableOperation insertOperation = TableOperation.Insert(report);
 				table.Execute(insertOperation);
 
 				// Cookie stays alive until user closes browser
 				HttpCookie userReport = new HttpCookie("userReport");
-				userReport["guid"] = guid;
+				userReport["guid"] = report.Guid;
 				Response.Cookies.Add(userReport);
 
 				return RedirectToAction("Details", "Report");
@@ -101,26 +81,17 @@ namespace Lisa.Kiwi.Web.Reporting.Controllers
 
 			if (ModelState.IsValid)
 			{
-				CloudTable table = GetTableStorage();
+                return View();
+            }
+			
+            CloudTable table = GetTableStorage();
+			TableOperation retrieveOperation = TableOperation.Retrieve<OriginalReport>(guid, "");
+			TableResult retrievedResult = table.Execute(retrieveOperation);
+			OriginalReport updateEntity = (OriginalReport) retrievedResult.Result;
 
-				TableOperation retrieveOperation = TableOperation.Retrieve<OriginalReport>(guid, "");
-				TableResult retrievedResult = table.Execute(retrieveOperation);
+            UpdateDetails(updateEntity, data, table);
 
-				OriginalReport updateEntity = (OriginalReport) retrievedResult.Result;
-
-				if (retrievedResult.Result != null)
-				{
-					updateEntity.Location = data.Location;
-					updateEntity.Time = data.Time;
-					updateEntity.Description = data.Description;
-
-					TableOperation insertOrReplaceOperation = TableOperation.InsertOrReplace(updateEntity);
-					table.Execute(insertOrReplaceOperation);
-
-					return RedirectToAction("ContactDetails", "Report");
-				}
-			}
-			return View();
+            return RedirectToAction("ContactDetails", "Report");
 		}
 
 		public ActionResult ContactDetails()
@@ -134,60 +105,37 @@ namespace Lisa.Kiwi.Web.Reporting.Controllers
 			HttpCookie cookie = HttpContext.Request.Cookies["userReport"];
 			string guid = cookie.Values["guid"];
 
-			if (ModelState.IsValid)
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+
+			CloudTable table = GetTableStorage();
+			TableOperation retrieveOperation = TableOperation.Retrieve<OriginalReport>(guid, "");
+			TableResult retrievedResult = table.Execute(retrieveOperation);
+			OriginalReport entity = (OriginalReport) retrievedResult.Result;
+
+			if (retrievedResult != null)
 			{
-				CloudTable table = GetTableStorage();
-
-				TableOperation retrieveOperation = TableOperation.Retrieve<OriginalReport>(guid, "");
-				TableResult retrievedResult = table.Execute(retrieveOperation);
-
-				OriginalReport entity = (OriginalReport) retrievedResult.Result;
-
-				if (retrievedResult != null)
+                var report = new WebApi.Report
 				{
-                    var report = new WebApi.Report
-					{
-						Description = entity.Description,
-						Created = entity.Created,
-						Location = entity.Location,
-						Time = entity.Time,
-						Guid = entity.PartitionKey,
-						Type = (ReportType)Enum.Parse(typeof(ReportType), entity.Type)
-					};
-                    var reportEntity = await _reportProxy.AddManualReport(report);
+					Description = entity.Description,
+					Created = entity.Created,
+					Location = entity.Location,
+					Time = entity.Time,
+					Guid = entity.PartitionKey,
+					Type = (ReportType)Enum.Parse(typeof(ReportType), entity.Type)
+				};
 
-                    //var reportEntity = _reportProxy.GetReports();
-                    //var getReport = reportEntity.Where(r => r.Guid == guid).FirstOrDefault();
-
-                    if (reportEntity != null)
-					{
-						var status = new Status
-						{
-							Created = entity.Created,
-							Name = StatusName.Open,
-                            Report = reportEntity.Id
-						};
-						_statusProxy.AddStatus(status);
-
-                        if (data.Name != null && (data.Email != null || data.PhoneNumber != null || data.StudentNumber == 0))
-                        {
-                            var contact = new Contact
-                            {
-                                Name = data.Name,
-                                EmailAddress = data.Email,
-                                PhoneNumber = data.PhoneNumber,
-                                StudentNumber = data.StudentNumber,
-                                Report = reportEntity.Id
-                            };
-                            _contactProxy.AddContact(contact);
-                        }
-					}
-
-
-					return RedirectToAction("Confirmed", "Report");
+                var reportEntity = await _reportProxy.AddManualReport(report);
+                if (reportEntity != null)
+				{
+					CreateStatus(entity.Created, reportEntity.Id);
+                    CreateContact(data, reportEntity.Id);
 				}
+				return RedirectToAction("Confirmed", "Report");
 			}
-			return View();
+            return View();
 		}
 
 		public ActionResult Confirmed()
@@ -202,6 +150,72 @@ namespace Lisa.Kiwi.Web.Reporting.Controllers
 
 			return View(entity);
 		}
+
+        private List<SelectListItem> GetReportTypes()
+        {
+            var types = Enum.GetValues(typeof(ReportType)).Cast<ReportType>();
+            List<SelectListItem> reportTypes = new List<SelectListItem>();
+            foreach (var reportType in types)
+            {
+                reportTypes.Add(new SelectListItem
+                {
+                    Text = reportType.GetReportTypeDisplayNameFromMetadata(),
+                    Value = reportType.ToString()
+                });
+            }
+            return reportTypes;
+        }
+
+        private OriginalReport DefineReport(ReportType reportType)
+        {
+            OriginalReport report = new OriginalReport();
+            var guid = Guid.NewGuid().ToString();
+
+            report.Guid = guid;
+            report.Time = DateTime.UtcNow;
+            report.Type = reportType.ToString();
+            report.PartitionKey = guid;
+            report.RowKey = "";
+
+            return report;
+        }
+
+        private void UpdateDetails(OriginalReport updateEntity, OriginalReport data, CloudTable table)
+        {
+            updateEntity.Location = data.Location;
+            updateEntity.Time = data.Time;
+            updateEntity.Description = data.Description;
+
+            TableOperation insertOrReplaceOperation = TableOperation.InsertOrReplace(updateEntity);
+            table.Execute(insertOrReplaceOperation);
+        }
+
+        private void CreateStatus(DateTime created, int id) 
+        {
+            var status = new Status
+					            {
+						            Created = created,
+						            Name = StatusName.Open,
+                                    Report = id
+					            };
+					            _statusProxy.AddStatus(status);
+        }
+
+        private void CreateContact(ContactMetadata data, int id) 
+        {
+            if (data.Name != null && (data.Email != null || data.PhoneNumber != null || data.StudentNumber == 0))
+            {
+                var contact = new Contact
+                {
+                    Name = data.Name,
+                    EmailAddress = data.Email,
+                    PhoneNumber = data.PhoneNumber,
+                    StudentNumber = data.StudentNumber,
+                    Report = id
+                };
+                _contactProxy.AddContact(contact);
+            }
+        }
 
 		private readonly ReportProxy _reportProxy = new ReportProxy(ConfigHelper.GetODataUri());
         private readonly ContactProxy _contactProxy = new ContactProxy(ConfigHelper.GetODataUri());
