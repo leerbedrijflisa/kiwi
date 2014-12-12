b47186c5dbb0d8a780e1f5994f68798fd5e138a5
Jordi Scharloo <jordischarloo@hotmail.com>
2014-12-12 15:36:15 +0100

    Add bundles and minification to wizard

---
+++ Lisa.Kiwi/Lisa.Kiwi.Web.Reporting/App_Start/BundleConfig.cs
@@ -0,0 +1,49 @@
+﻿using System.Web.Optimization;
+
+namespace Lisa.Kiwi.Web.Reporting
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/bundles/css")
+                .Include("~/Content/bootstrap.css", new CssRewriteUrlTransform())
+                .Include("~/Content/site.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Content/themes/base/jquery.ui.button.css",
+                        "~/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Content/themes/base/jquery.ui.slider.css",
+                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Content/themes/base/jquery.ui.theme.css"));            
+
+            bundles.Add(new ScriptBundle("~/bundles/bottom").Include(
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/kiwi.js"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
--- Lisa.Kiwi/Lisa.Kiwi.Web.Reporting/Global.asax.cs
+++ Lisa.Kiwi/Lisa.Kiwi.Web.Reporting/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Lisa.Kiwi.Web.Reporting
@@ -10,6 +11,7 @@ namespace Lisa.Kiwi.Web.Reporting
 		{
 			AreaRegistration.RegisterAllAreas();
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
 		}
 	}
 }
