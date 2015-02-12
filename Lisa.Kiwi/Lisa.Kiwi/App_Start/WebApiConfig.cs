0f65098c644cc798df87245e97b119ff821769ce
Kevin van Hulst <kevin.van.hulst@gmail.com>
2015-02-12 14:46:48 +0100

    Moved database stuff to the Web API project

--- Lisa.Kiwi/Lisa.Kiwi/App_Start/WebApiConfig.cs
+++ Lisa.Kiwi/Lisa.Kiwi/App_Start/WebApiConfig.cs
@@ -1,8 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Web.Http;
-using System.Web.OData.Builder;
-using System.Web.OData.Extensions;
-using Lisa.Kiwi.Data;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
