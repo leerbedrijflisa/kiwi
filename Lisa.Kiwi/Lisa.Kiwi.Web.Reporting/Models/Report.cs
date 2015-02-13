fecf525de5429902d685621da4a59d7fb6b6ee4f
AndreasFurster <andreas.furster@hotmail.com>
2015-02-13 14:36:09 +0100

    Deleted models

---
+++ Lisa.Kiwi/Lisa.Kiwi.Web.Reporting/Models/Report.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Lisa.Kiwi.Web.Reporting.Models
+{
+	public class Report : TableEntity
+	{
+		public Report()
+		{
+			CreatedAt = DateTime.UtcNow;
+		}
+
+		public int Id { get; set; }
+		public string Building { get; set; }
+        public string DetailedLocation { get; set; }
+		public DateTime CreatedAt { get; set; }
+        public string ReporterName { get; set; }
+        public string ReporterTelephoneNumber { get; set; }
+        public string ReporterEmail { get; set; }
+        public string ReporterComment { get; set; }
+	}
+}
