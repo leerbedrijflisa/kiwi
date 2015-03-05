e1aa90d88835529149dd20385ab58d0a0c2efb0d
Bryan Stekelenburg <xerxesmeow@gmail.com>
2015-03-05 11:07:39 +0100

    Added ContactRequired View Model

---
+++ Lisa.Kiwi/Lisa.Kiwi.Web/ViewModels/ContactRequiredViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lisa.Kiwi.Web.ViewModels
+{
+    public class ContactRequiredViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+    }
+}
