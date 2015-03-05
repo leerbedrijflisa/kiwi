24117d71b12e19322b777d0485504b63dd34545d
Bryan Stekelenburg <xerxesmeow@gmail.com>
2015-03-05 09:50:57 +0100

    Updated namespaces in the view models

--- Lisa.Kiwi/Lisa.Kiwi.Web/ViewModels/TheftViewModel.cs
+++ Lisa.Kiwi/Lisa.Kiwi.Web/ViewModels/TheftViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace Lisa.Kiwi.Web.ViewModels
+namespace Lisa.Kiwi.Web
 {
     public class TheftViewModel
     {
