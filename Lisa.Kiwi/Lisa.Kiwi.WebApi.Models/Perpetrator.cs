e5ac69d6ce1b6ad9d159cc17a5163d0babd471fd
AndreasFurster <andreas.furster@hotmail.com>
2015-03-11 15:33:48 +0100

    Gender to Sex in Web Api

--- Lisa.Kiwi/Lisa.Kiwi.WebApi.Models/Perpetrator.cs
+++ Lisa.Kiwi/Lisa.Kiwi.WebApi.Models/Perpetrator.cs
@@ -7,7 +7,7 @@ namespace Lisa.Kiwi.WebApi
     public class Perpetrator
     {
         public string Name { get; set; }
-        public int? Sex { get; set; }
+        public SexEnum? Sex { get; set; }
         public string SkinColor { get; set; }
         public string Clothing { get; set; }
         public int? MinimumAge { get; set; }
