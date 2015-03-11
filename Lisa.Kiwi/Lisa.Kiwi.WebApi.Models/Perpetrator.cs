7db73fb152cf58eb57869fe42248d59230fd61ef
Bryan Stekelenburg <xerxesmeow@gmail.com>
2015-03-11 09:43:45 +0100

    Fixed perpetrator model

--- Lisa.Kiwi/Lisa.Kiwi.WebApi.Models/Perpetrator.cs
+++ Lisa.Kiwi/Lisa.Kiwi.WebApi.Models/Perpetrator.cs
@@ -7,11 +7,11 @@ namespace Lisa.Kiwi.WebApi
     public class Perpetrator
     {
         public string Name { get; set; }
-        public int Sex { get; set; }
+        public int? Sex { get; set; }
         public string SkinColor { get; set; }
         public string Clothing { get; set; }
-        public int MinimumAge { get; set; }
-        public int MaximumAge { get; set; }
+        public int? MinimumAge { get; set; }
+        public int? MaximumAge { get; set; }
         public string UniqueProperties { get; set; }
     }
 }
