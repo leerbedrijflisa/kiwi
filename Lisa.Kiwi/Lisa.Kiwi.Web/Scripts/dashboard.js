bdec568eb1fe556de2c9edd8a9f28cbbc1bb190b
Andreas Furster <andreas.furster@hotmail.com>
2015-05-20 16:29:59 +0200

    Integrating signalR

---
+++ Lisa.Kiwi/Lisa.Kiwi.Web/Scripts/dashboard.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    var reports = $.connection.reportsHub;
+
+    reports.client.UpdateReport = function (report) {
+        console.log(report);
+    };
+    $.connection.hub.url = window.ApiUrl + "signalr";
+
+    
+    $.connection.hub.start().done(function () {
+        
+    });
+});
---
+++ Lisa.Kiwi/Lisa.Kiwi.WebApi/Hubs/ReportsHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace Lisa.Kiwi.WebApi.Hubs
+{
+    [Authorize]
+    public class ReportsHub : Hub
+    {
+        public void UpdateReport(Report report)
+        {
+            Clients.All.updateReport(report);
+        }
+    }
+}
