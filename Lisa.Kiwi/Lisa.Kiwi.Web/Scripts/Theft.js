26836a7677c3a3f67f056ea92b7b662104fdaadf
Bryan Stekelenburg <xerxesmeow@gmail.com>
2015-05-21 13:48:42 +0200

    Datetimepicker has today default as 0, so NaN resulted in today.

--- Lisa.Kiwi/Lisa.Kiwi.Web/Scripts/Theft.js
+++ Lisa.Kiwi/Lisa.Kiwi.Web/Scripts/Theft.js
@@ -1,14 +1,13 @@
 ﻿$(function () {
     var dt = new Date();
     var maxDate = dt.getFullYear() + '/' + dt.getMonth() + '/' + dt.getDate();
-    var timeStop = dt.getHours() + ':' + dt.getMinutes()
 
-    //the +maxDate and +timeStop are to set the maxTime and maxDate to the right dates and times, else they will be one minute/day less.
+    //Default for today is 0
     $('#DateOfTheft').datetimepicker({
-        maxDate: +maxDate,
+        maxDate: 0,
         dateFormat: 'mm/dd/Y H:i',
-        defaultTime: timeStop,
-        maxTime: +timeStop,
+        defaultTime: 0,
+        maxTime: 0,
         step: 1,
         lang: 'nl',
         timepickerScrollbar: false,
@@ -17,7 +16,7 @@
             dateCheck = dp.getFullYear() + '/' + dp.getMonth() + '/' + dp.getDate();
             if (dateCheck == maxDate)
             {
-                this.setOptions({ maxTime: +timeStop });
+                this.setOptions({ maxTime:  0});
             }
             if(dateCheck != maxDate)
             {
