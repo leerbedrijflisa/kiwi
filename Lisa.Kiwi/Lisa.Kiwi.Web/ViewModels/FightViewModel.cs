924fb907faaceb1512083c92b403f6817e5747e8
Kevin van Hulst <kevin.van.hulst@gmail.com>
2015-05-28 15:43:00 +0200

    Added an asterisk to the fight viewmodel

--- Lisa.Kiwi/Lisa.Kiwi.Web/ViewModels/FightViewModel.cs
+++ Lisa.Kiwi/Lisa.Kiwi.Web/ViewModels/FightViewModel.cs
@@ -5,7 +5,7 @@ namespace Lisa.Kiwi.Web
 {
     public class FightViewModel
     {
-        [DisplayName("Hoeveel personen zijn er aan het vechten?")]
+        [DisplayName("Hoeveel personen zijn er aan het vechten?*")]
         public int FighterCount { get; set; }
 
         [Required]
