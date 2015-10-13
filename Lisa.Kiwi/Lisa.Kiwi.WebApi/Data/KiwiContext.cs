8bbdfdc39d7139d830c3b9e45ca0b71c85a663e8
Kevin van Hulst <kevin.van.hulst@gmail.com>
2015-10-13 10:56:19 +0200

    remove the unused Vehicles, Locations and Contacts DbSet

--- Lisa.Kiwi/Lisa.Kiwi.WebApi/Data/KiwiContext.cs
+++ Lisa.Kiwi/Lisa.Kiwi.WebApi/Data/KiwiContext.cs
@@ -12,9 +12,6 @@ namespace Lisa.Kiwi.WebApi
         }
 
         public DbSet<ReportData> Reports { get; set; }
-        public DbSet<VehicleData> Vehicles { get; set; }
-        public DbSet<LocationData> Locations { get; set; }
-        public DbSet<ContactData> Contacts { get; set; }
 
         public bool HasUnsavedChanges()
         {
