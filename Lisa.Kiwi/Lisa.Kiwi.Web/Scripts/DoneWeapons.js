e2c0d32de83f329e3c900b853a5be9f3a8d247fa
Kevin van Hulst <kevin.van.hulst@gmail.com>
2015-10-28 13:30:53 +0100

    fixed the weapon field on the done page

--- Lisa.Kiwi/Lisa.Kiwi.Web/Scripts/DoneWeapons.js
+++ Lisa.Kiwi/Lisa.Kiwi.Web/Scripts/DoneWeapons.js
@@ -1,4 +1,14 @@
-﻿$(document).ready(function() {
+﻿$(window).load(function () {
+    if ($("select[name=WeaponType]").val() === "Anders") {
+        $("input[name=WeaponTypeOther]").parent().show();
+    }
+    else {
+        $("input[name=WeaponTypeOther]").parent().hide();
+        $("input[name=WeaponTypeOther]").val("");
+    }
+});
+
+$(document).ready(function () {
     $("select[name=WeaponType]").change(function() {
         if ($(this).val() === "Anders") {
             $("input[name=WeaponTypeOther]").parent().slideDown(300);
