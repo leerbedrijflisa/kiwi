4bbfbe5e9c1bc65782ecc6547dc1b4a5b6e58758
Rik Pronk <rikpronkie@gmail.com>
2015-10-29 11:07:06 +0100

    Put the script tags in a seperate script file, cleaned up the code a bit, fixed console error.

--- Lisa.Kiwi/Lisa.Kiwi.Web/App_Start/BundleConfig.cs
+++ Lisa.Kiwi/Lisa.Kiwi.Web/App_Start/BundleConfig.cs
@@ -9,10 +9,12 @@ namespace Lisa.Kiwi.Web
             bundles.Add(new ScriptBundle("~/bundles/scripts")
                 .Include("~/Scripts/bug-report.js")
                 .Include("~/Scripts/messageReciever.js")
-                .Include("~/Scripts/header-resizer.js"));
+                .Include("~/Scripts/header-resizer.js")
+                .Include("~/Scripts/headerCategory.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/frameScripts")
-                .Include("~/Scripts/messageSender.js"));
+                .Include("~/Scripts/messageSender.js")
+                .Include("~/Scripts/headerCategory.js"));
 
             bundles.Add(new StyleBundle("~/bundles/styles")
                 .Include("~/Content/reporter.css"));
---
+++ Lisa.Kiwi/Lisa.Kiwi.Web/Scripts/headerCategory.js
@@ -0,0 +1,19 @@
+﻿function isNullOrWhitespace(variable) {
+    return variable.length === 0
+        || !variable.trim()
+        || variable === null
+        || typeof (variable) === "undefined";
+}
+
+if (typeof (Storage) !== "undefined")
+{
+    if (localStorage.Category !== category && !isNullOrEmpty(category)) {
+        localStorage.Category = category;
+    } else {
+        // Sorry! No Web Storage support...
+    }
+
+    if (!isNullOrEmpty(localStorage.Category) && !$('#iframe').length) {
+        $('legend').first().prepend(localStorage.Category + " - ");
+    }
+}
