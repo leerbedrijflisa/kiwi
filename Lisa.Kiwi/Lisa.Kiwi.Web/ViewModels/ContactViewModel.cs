60a3deca6e4a21acb36db941cd755e2483fdcd4d
Kevin van Hulst <kevin.van.hulst@gmail.com>
2015-10-30 11:01:03 +0100

    fixed validation for the contact pages, there is a weird layout bug though

--- Lisa.Kiwi/Lisa.Kiwi.Web/ViewModels/ContactViewModel.cs
+++ Lisa.Kiwi/Lisa.Kiwi.Web/ViewModels/ContactViewModel.cs
@@ -8,13 +8,15 @@ namespace Lisa.Kiwi.Web
     {
         public string Name { get; set; }
 
-        [RequiredIf("IsNullOrWhiteSpace(EmailAddress)", ErrorMessageResourceName = "PhoneOrEmail", ErrorMessageResourceType = typeof(ErrorMessages))]
+        [RequiredIf("Enabled && IsNullOrWhiteSpace(EmailAddress)", ErrorMessageResourceName = "PhoneOrEmail", ErrorMessageResourceType = typeof(ErrorMessages))]
         [MinLength(4, ErrorMessageResourceName = "PhoneNumber", ErrorMessageResourceType = typeof(ErrorMessages))]
         public string PhoneNumber { get; set; }
 
-        [RequiredIf("IsNullOrWhiteSpace(PhoneNumber)", ErrorMessageResourceName = "PhoneOrEmail", ErrorMessageResourceType = typeof(ErrorMessages))]
+        [RequiredIf("Enabled && IsNullOrWhiteSpace(PhoneNumber)", ErrorMessageResourceName = "PhoneOrEmail", ErrorMessageResourceType = typeof(ErrorMessages))]
         [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessageResourceName = "Email", ErrorMessageResourceType = typeof(ErrorMessages))]
         [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
+
+        public bool Enabled { get; set; }
     }
 }
