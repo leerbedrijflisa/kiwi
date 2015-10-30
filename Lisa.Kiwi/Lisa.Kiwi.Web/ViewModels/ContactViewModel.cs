f7d76b34c07ecf815d5a31d62c741e3aa005b05c
Kevin van Hulst <kevin.van.hulst@gmail.com>
2015-10-30 09:29:47 +0100

    fixed the validation for the contact page

--- Lisa.Kiwi/Lisa.Kiwi.Web/ViewModels/ContactViewModel.cs
+++ Lisa.Kiwi/Lisa.Kiwi.Web/ViewModels/ContactViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using ExpressiveAnnotations.Attributes;
 using Lisa.Kiwi.Web.Resources;
 
 namespace Lisa.Kiwi.Web
@@ -7,9 +8,11 @@ namespace Lisa.Kiwi.Web
     {
         public string Name { get; set; }
 
+        [RequiredIf("IsNullOrWhiteSpace(EmailAddress)", ErrorMessageResourceName = "PhoneOrEmail", ErrorMessageResourceType = typeof(ErrorMessages))]
         [MinLength(4, ErrorMessageResourceName = "PhoneNumber", ErrorMessageResourceType = typeof(ErrorMessages))]
         public string PhoneNumber { get; set; }
 
+        [RequiredIf("IsNullOrWhiteSpace(PhoneNumber)", ErrorMessageResourceName = "PhoneOrEmail", ErrorMessageResourceType = typeof(ErrorMessages))]
         [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessageResourceName = "Email", ErrorMessageResourceType = typeof(ErrorMessages))]
         [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
