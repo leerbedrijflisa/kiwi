bc28842e1d7c2d8101974fd5a7e9fcdf23e656f8
William Willing <william@williamwilling.com>
2016-07-07 15:40:24 +0200

    Added code dependencies directly to Lisa.Kiwi.Web project in an attempt to get Azure to play along.

---
+++ Lisa.Kiwi/Lisa.Kiwi.Web/Dependencies/Access/Models/Token.cs
@@ -0,0 +1,14 @@
+﻿namespace Lisa.Common.Access
+{
+    public class Token
+    {
+        public Token()
+        {
+            Type = "bearer";
+        }
+
+        public string Value { get; set; }
+        public string Type { get; set; }
+        public int ExpiresIn { get; set; }
+    }
+}
---
+++ Lisa.Kiwi/Lisa.Kiwi.Web/Dependencies/Proxies/AuthenticationProxy.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Lisa.Common.Access;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Lisa.Kiwi.WebApi.Access
+{
+    public class AuthenticationProxy
+    {
+        public AuthenticationProxy(string baseUrl, string resourceUrl)
+        {
+            _resourceUrl = resourceUrl;
+            _client = new HttpClient {BaseAddress = new Uri(baseUrl)};
+            _baseUrl = baseUrl;
+        }
+
+        // TODO: replace the two functions underneath with a function with which you can request
+        // the claims of the current user.
+
+        public async Task<bool> GetIsAdmin(string tokenType, string token)
+        {
+            _client.DefaultRequestHeaders.Add("Authorization", String.Format("{0} {1}", tokenType, token));
+
+            var response = await _client.GetAsync("/api/users/is_admin");
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new WebApiException("Unexpected statuscode", response.StatusCode);
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            return Boolean.Parse(content);
+        }
+
+        public async Task<bool> GetIsAnonymous(string tokenType, string token)
+        {
+            _client.DefaultRequestHeaders.Add("Authorization", String.Join(" ", tokenType, token));
+
+            var response = await _client.GetAsync("/api/users/is_anonymous");
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new WebApiException("Unexpected statuscode", response.StatusCode);
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return Boolean.Parse(content);
+        }
+
+        public async Task<Token> Login(string userName, string password)
+        {
+            var requestContent = String.Format(
+                "grant_type=password&username={0}&password={1}",
+                HttpUtility.UrlEncode(userName),
+                HttpUtility.UrlEncode(password));
+
+            return await Login(requestContent);
+        }
+
+        public async Task<Token> LoginAnonymous(string anonymousToken)
+        {
+            var requestContent = String.Format(
+                "grant_type=anonymous&token={0}",
+                HttpUtility.UrlEncode(anonymousToken));
+
+            return await Login(requestContent);
+        }
+
+        public async Task UpdatePassword(string id, string password, string tokenType, string token)
+        {
+            _client.DefaultRequestHeaders.Add("Authorization", String.Join(" ", tokenType, token));
+
+            var request = new HttpRequestMessage
+            {
+                Method = new HttpMethod("PATCH"),
+                RequestUri = new Uri(String.Format("{0}/{1}?id={2}", _baseUrl, _resourceUrl, id)),
+                Content = new StringContent(JsonConvert.SerializeObject(new { password = password }), Encoding.UTF8, "Application/json")
+            };
+
+            var response = await _client.SendAsync(request);
+            if (response.StatusCode == HttpStatusCode.Forbidden || response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                throw new UnauthorizedAccessException();
+            }
+        }
+
+        private async Task<Token> Login(string requestContent)
+        {
+            var response = await _client.PostAsync(_resourceUrl, new StringContent(requestContent));
+            switch (response.StatusCode)
+            {
+                case HttpStatusCode.BadRequest:
+                    return null;
+
+                case HttpStatusCode.OK:
+                    var resultContent = await response.Content.ReadAsStringAsync();
+
+                    try
+                    {
+
+                        var authInfo = JObject.Parse(resultContent);
+                        return new Token
+                        {
+                            Value = authInfo.SelectToken("access_token").ToString(),
+                            Type = authInfo.SelectToken("token_type").ToString(),
+                            ExpiresIn = authInfo.SelectToken("expires_in").Value<int>(),
+                            Role = authInfo.SelectToken("role").ToString()
+                        };
+                    }
+                    catch (Exception e)
+                    {
+                        throw new WebApiException("The response body was malformed. Either the JSON is incorrect or a token property is missing.", response.StatusCode, e);
+                    }
+
+                default:
+                    throw new WebApiException("Unexpected HTTP status code. Only 200 OK and 400 Bad Request are acceptable.", response.StatusCode);
+            }
+        }
+
+        private readonly HttpClient _client;
+        private readonly string _resourceUrl;
+        private readonly string _baseUrl;
+    }
+}
